218b23952ef5f2cae388589f59699f86c4392935
DhavalKotak <DhavalKotak@users.noreply.github.com>
2021-12-05 14:12:33 +0530

    accepting req route added

--- data/friend/friendRequest.js
+++ data/friend/friendRequest.js
@@ -46,6 +46,7 @@ router.post('/declineRequest', (req, res) => {
                     console.log(results)
                     res
                     .status(200)
+                    .json(results)
                 }
             })
         }else{
@@ -54,6 +55,38 @@ router.post('/declineRequest', (req, res) => {
         }
     })
 
+})
+router.post('/acceptRequest', (req, res) => {
+    const username = req.body.username
+    const sender = req.body.sender
+    const authHeader = req.headers.authorization
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if(err){
+            res.json({message: "Invalid Token!"})
+        }else if(user.user == username){
+            db.query(`INSERT INTO FRIENDLIST VALUES('${username}','${sender}')`,(err, results) => {
+                if(err){
+                    res.status(500)
+                    console.log(err)
+                }else{
+                    console.log(results)
+                }
+            })
+            db.query(`DELETE FROM FRIENDREQUEST WHERE SENDER = '${sender}' AND RECEIVER = '${username}'`,(err, results) => {
+                if(err)
+                    res.status(500)
+                else{
+                    console.log(results)
+                    res
+                    .status(200)
+                    .json(results)
+                }
+            })
+        }else{
+            res.json({message: "Stolen Token"})
+        }
+    })
 })
 
 module.exports = router
